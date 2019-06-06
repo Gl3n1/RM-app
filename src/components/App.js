a10c5c4f77bd1b704be2c52982009b0c39bc90ee
Glen <glenlexry@seekasia.com>
2019-06-06 18:58:03 +0800

    update

--- src/components/App.js
+++ src/components/App.js
@@ -8,9 +8,14 @@ import FollowUpCard from './Cards/FollowUpCard/index.jsx';
 import TopNewsCard from './Cards/TopNewsCard/index.jsx';
 import ShowMoreCard from './Cards/ShowMoreCard/index.jsx';
 import BottomNavbar from './BottomNavbar';
+import ProfessionalCard from './Cards/ProfessionalCard/index.jsx';
+import EmptySpace from './Empty'
 
-import hsbcimg from '../images/s3-news.jpg'
-import hsbcimg2 from '../images/0.jpeg'
+import hsbcimg from '../images/s3-news.jpg';
+import hsbcimg2 from '../images/0.jpeg';
+import property from '../images/property.jpg';
+import mily from '../images/g.jpeg';
+import milyimg from '../images/mily1.png';
 
 function App() {
   return (
@@ -28,11 +33,11 @@ function App() {
       <section>
         <h2 className='title'>Top News</h2>
           <TopNewsCard 
-            link='https://www.thedrum.com/news/2019/05/31/hsbc-champions-internationalism-football-full-english-final-ad'
-            title='Tara Latini likes this'
-            date='31 May 2019'
-            desc='HSBC UK has championed internationalism in football to celebrate the first all English Champions League and Europa League finals.'
-            hasImage='false'
+            link='https://www.thestar.com.my/business/business-news/2019/05/08/malaysians-spend-more-time-looking-at-properties-than-going-to-the-gym-or-reading/'
+            title='Malaysians spend more time looking at properties...'
+            date='Wednesday, 8 May 2019'
+            desc='...watching the news,” said country head for retail banking and wealth management Tara Latini.'
+            img={property}
           />
           <TopNewsCard 
             link='https://www.thedrum.com/news/2019/05/31/hsbc-champions-internationalism-football-full-english-final-ad'
@@ -52,6 +57,16 @@ function App() {
       </section>
       <section>
         <h2 className='title'>Professional</h2>
+        <ProfessionalCard 
+          name='Mily Liang'
+          position='Head of Customer Value Management at HSBC Retail Banking and Wealth Management'
+          postTime='1mo'
+          desc='If you are passionate about leading change to our teams and customers, check out this great opportunity at HSBC Malaysia.'
+          profileImg={mily}
+          img={milyimg}
+          imgDesc='We are currently seeking for a subject matter expert in change delivery to lead...'
+          link='https://www.linkedin.com/feed/update/activity:6527024284886503424/'
+        />
       </section>
       <BottomNavbar />
     </div>
